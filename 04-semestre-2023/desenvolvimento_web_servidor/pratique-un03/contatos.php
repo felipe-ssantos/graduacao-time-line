0d650ef8fa9007334c68604146724b598bd2efe2
Felipe Santos <fecoder.dev@gmail.com>
2023-10-29 23:59:03 -0300

    :ok_hand: atualizacoes na página contatos.php
    Adicionado algumas melhorias na página de contato:
    
    - Agora o usuário pode selecionar apenas o ID e, em seguida, escolher o campo que deseja atualizar na tabela. Exemplo: ID 37 | Nome: Fulano agora ID: 37 nome: Ciclano. Antes era necessário selecionar o ID e, em seguida, preencher todo o formulário de atualização. Caso não fosse preenchido, os outros campos eram cadastrados em branco na tabela.
    
    - No formulário de atualização, também foi adicionada uma condição que não permite que o usuário atualize o mesmo e-mail para o mesmo contato.
    
    - O usuário também não pode atualizar um e-mail que já existe na tabela e que pertence a outro ID. Antes, o usuário podia ter o mesmo e-mail para IDs diferentes. Podem existir pessoas com nomes iguais, mas não com o mesmo e-mail.
    
    - O usuário não pode cadastrar o e-mail de um contato que já pertence a outro usuário. A chance de usuários diferentes terem o mesmo contato é baixa, mas pode acontecer. Caso isso ocorra, o usuário será avisado em uma página de erro.
    
    - Cada lista de contatos é individual, um usuário não tem acesso à lista de contatos de outro usuário.

--- 04-semestre-2023/desenvolvimento_web_servidor/pratique-un03/contatos.php
+++ 04-semestre-2023/desenvolvimento_web_servidor/pratique-un03/contatos.php
@@ -1,100 +1,136 @@
 <?php
-//verificar possíveis erros
+//Verificar possíveis erros
+/*
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+*/
 
 session_start();
 
 // Verificar se o usuário não está autenticado
 if (!isset($_SESSION['usuario_logado']) || !is_array($_SESSION['usuario_logado'])) {
-    // Se não estiver autenticado, redirecione para a página de login
-    header("Location: index.php");
+    header("Location: index.php"); // Redireciona para a página de login se não estiver autenticado
     exit();
 }
 
-// Obtém o ID do usuário a partir da sessão
 $idUsuario = $_SESSION['usuario_logado']['id_usuario'];
 
-// Conexão com o banco de dados MySQL
+/* Conexão com banco de dados. */
 $servername = "localhost";
-$username = "";
-$password = "";
-$dbname = "";
+$username = "nome-de-usuario";
+$password = " ";
+$dbname = " "; //nome banco de dados
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Verifica a conexão com o banco de dados
 if ($conn->connect_error) {
     die("Erro na conexão com o banco de dados: " . $conn->connect_error);
 }
 
-// Defina a codificação de caracteres para UTF-8
 $conn->set_charset("utf8mb4");
 
-// Verificar se há uma mensagem de erro na sessão
+// Exiba uma mensagem de erro de cadastro, se houver, e depois remove a mensagem da sessão.
 if (isset($_SESSION['erro_cadastro'])) {
     echo "<div class='alert alert-danger'>" . $_SESSION['erro_cadastro'] . "</div>";
-
-    // Limpar a sessão para que a mensagem não seja exibida novamente
     unset($_SESSION['erro_cadastro']);
 }
 
-// Verificação do envio do formulário de exclusão
+// Lógica para exclusão de contato
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["id_excluir"])) {
     $idExcluir = $_POST["id_excluir"];
 
-    // Consulta SQL para verificar se o ID existe antes de excluir
     $sqlVerificarID = "SELECT COUNT(*) as count FROM contatos WHERE id = $idExcluir AND id_usuario = $idUsuario";
     $resultVerificarID = $conn->query($sqlVerificarID);
     $rowVerificarID = $resultVerificarID->fetch_assoc();
 
     if ($rowVerificarID['count'] > 0) {
-        // Verifica primeiro se o ID existe na tabela, para depois realizar a exclusão.
+        // Se o ID do contato existe, execute a consulta SQL para excluir o contato
         $sqlExcluir = "DELETE FROM contatos WHERE id = $idExcluir AND id_usuario = $idUsuario";
         if ($conn->query($sqlExcluir) === TRUE) {
             echo "<div class='alert alert-success'>Contato excluído com sucesso!</div>";
         } else {
+            // Se o ID do contato não existe, exiba uma mensagem de erro
             echo "<div class='alert alert-danger'>Erro ao excluir o contato: " . $conn->error . "</div>";
         }
     } else {
-        // Caso o ID não exista na tabela, exiba uma mensagem de erro
         echo "<div class='alert alert-danger'>ID de contato não encontrado.</div>";
     }
 }
 
-// Verificação do envio do formulário de atualização
+// Lógica para atualização de contato
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["id"])) {
     $idAtualizar = $_POST["id"];
     $novoNome = $_POST["novo_nome"];
     $novoSobrenome = $_POST["novo_sobrenome"];
     $novoEmail = $_POST["novo_email"];
+    $erro = ''; // Variável para armazenar mensagens de erro
 
-    // Consulta SQL para verificar se o ID existe antes de atualizar
-    $sqlVerificarID = "SELECT COUNT(*) as count FROM contatos WHERE id = $idAtualizar AND id_usuario = $idUsuario";
+    // Verificar se o ID do contato existe
+    $sqlVerificarID = "SELECT COUNT(*) as count, email FROM contatos WHERE id = $idAtualizar AND id_usuario = $idUsuario";
     $resultVerificarID = $conn->query($sqlVerificarID);
     $rowVerificarID = $resultVerificarID->fetch_assoc();
 
     if ($rowVerificarID['count'] > 0) {
-        // Se o ID existe na tabela, então podemos prosseguir com a atualização
-        $sqlAtualizar = "UPDATE contatos SET nome = '$novoNome', sobrenome = '$novoSobrenome', email = '$novoEmail' WHERE id = $idAtualizar AND id_usuario = $idUsuario";
-        if ($conn->query($sqlAtualizar) === TRUE) {
-            echo "<div class='alert alert-success'>Contato atualizado com sucesso!</div>";
+        $setClauses = array();
+
+        // Verificar se um novo nome ou sobrenome foi fornecido e, se sim, atualizar o nome no banco de dados.
+        if (!empty($novoNome)) {
+            $setClauses[] = "nome = '$novoNome'";
+        }
+
+        if (!empty($novoSobrenome)) {
+            $setClauses[] = "sobrenome = '$novoSobrenome'";
+        }
+        //fim condicao verificar 'nome e sobrenome'
+
+        if (!empty($novoEmail)) {
+            // Verificar se o novo email é diferente do email atual
+            if ($novoEmail != $rowVerificarID['email']) {
+                // Verificar se o novo email já existe na tabela para o usuário atual
+                $sqlVerificarEmail = "SELECT COUNT(*) as count FROM contatos WHERE email = '$novoEmail' AND id_usuario = $idUsuario";
+                $resultVerificarEmail = $conn->query($sqlVerificarEmail);
+                $rowVerificarEmail = $resultVerificarEmail->fetch_assoc();
+
+                if ($rowVerificarEmail['count'] == 0) {
+                    // Se o email não pertence a outro contato, atualize o campo desejado
+                    $setClauses[] = "email = '$novoEmail'";
+                } else {
+                    // Se o email pertence a outro contato na tabela, defina uma mensagem de erro
+                    $erro = "Este email já pertence a outro contato na tabela.";
+                }
+            } else {
+                // Se o email é o mesmo que o email atual, defina uma mensagem de erro
+                $erro = "O email atualizado é igual ao email existente na tabela.";
+            }
+        }
+
+        if (empty($erro)) {
+            // Se não houver erros ou se apenas nome, sobrenome ou email for atualizado, construa a consulta SQL de atualização
+            $sqlAtualizar = "UPDATE contatos SET " . implode(', ', $setClauses) . " WHERE id = $idAtualizar AND id_usuario = $idUsuario";
+
+            if ($conn->query($sqlAtualizar) === TRUE) {
+                echo "<div class='alert alert-success'>Contato atualizado com sucesso!</div>";
+            } else {
+                echo "<div class='alert alert-danger'>Erro ao atualizar o contato: " . $conn->error . "</div>";
+            }
         } else {
-            echo "<div class='alert alert-danger'>Erro ao atualizar o contato: " . $conn->error . "</div>";
+            // Exibir mensagem de erro para o usuário
+            echo "<div class='alert alert-danger'>$erro</div>";
         }
     } else {
-        // Se o ID não existe na tabela, exiba uma mensagem de erro
         echo "<div class='alert alert-danger'>ID de contato não encontrado.</div>";
     }
 }
 
-// Verificação do envio do formulário de cadastro
+
+// Lógica para cadastro de novo contato
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["nome_contato"]) && isset($_POST["sobrenome_contato"]) && isset($_POST["email_contato"])) {
     $nomeContato = $_POST["nome_contato"];
     $sobrenomeContato = $_POST["sobrenome_contato"];
     $emailContato = $_POST["email_contato"];
 
-    // Consulta SQL para verificar se o email já existe na tabela
     $sqlVerificarEmail = "SELECT COUNT(*) as count FROM contatos WHERE email = '$emailContato' AND id_usuario = $idUsuario";
     $resultVerificarEmail = $conn->query($sqlVerificarEmail);
     $rowVerificarEmail = $resultVerificarEmail->fetch_assoc();
@@ -103,7 +139,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["nome_contato"]) && iss
         // Se o email já existe na tabela, exiba uma mensagem de erro
         echo "<div id='mensagemErro' class='alert alert-danger' style='display:none;'>Este email já foi cadastrado.</div>";
     } else {
-        // Se o email não existe na tabela, insira o novo contato
+        // Se o email não existe na tabela, insira um novo contato
         $sqlInserirContato = "INSERT INTO contatos (id_usuario, nome, sobrenome, email) VALUES ($idUsuario, '$nomeContato', '$sobrenomeContato', '$emailContato')";
         if ($conn->query($sqlInserirContato) === TRUE) {
             echo "<div class='alert alert-success'>Contato cadastrado com sucesso!</div>";
@@ -116,8 +152,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["nome_contato"]) && iss
 // Consulta SQL para selecionar todos os registros da tabela "contatos" do usuário
 $sql = "SELECT * FROM contatos WHERE id_usuario = '$idUsuario'";
 $result = $conn->query($sql);
-
-// Exibir tabela de contatos
+/* Cabeçalho da tabela para listar os contatos */
 echo "<h1 style='text-align: center; margin: 20px 0 20px 0; font-family: \"Roboto\", sans-serif; font-weight: 700;'>Lista de contatos cadastrados</h1>";
 echo "<table class='table table-bordered'>";
 echo "<thead class='table-dark'>";
@@ -131,6 +166,7 @@ echo "</tr>";
 echo "</thead>";
 echo "<tbody>";
 
+/* Loop para exibir os contatos na tabela com opção de exclusão */
 while ($row = $result->fetch_assoc()) {
     echo "<tr>";
     echo "<td>" . $row["id"] . "</td>";
@@ -148,9 +184,9 @@ while ($row = $result->fetch_assoc()) {
 
 echo "</tbody></table>";
 
-// Exibe o botão "Sair do sistema"
+//Formulário do botão para sair do sistema
 echo '
- <div class="logout-button">
+<div class="logout-button">
     <form method="post" action="logout.php">
         <button type="submit" class="btn btn-danger">Sair do sistema</button>
     </form>
@@ -169,19 +205,43 @@ $conn->close();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <title>Contatos</title>
 
+    <!-- Validação de dados: nome & sobrenome -->
+    <script>
+    function validarFormulario() {
+        // Obtém os valores dos campos de nome e sobrenome
+        var nome = document.getElementById("nome_contato").value;
+        var sobrenome = document.getElementById("sobrenome_contato").value;
+        
+        // Define expressões regulares (/^[a-zA-Z\s]+$/) para verificar se os valores contêm apenas letras
+        var nomeValido = /^[a-zA-Z]+$/.test(nome);
+        var sobrenomeValido = /^[a-zA-Z\s]+$/.test(sobrenome); 
+
+        // Verifica se os campos de nome e sobrenome são válidos
+        if (!nomeValido || !sobrenomeValido) {            
+            alert("Os campos de nome e sobrenome devem conter apenas letras."); //exibe um alerta, caso os campos não forem válidos.
+            return false;
+        }
+
+        return true; //se verdadeiro retorne true.
+    }
+    </script>
+
     <style>
         * {
             font-family: 'Roboto', sans-serif;
         }
 
         /* Estilo para o link no rodapé */
-        footer a {
-            text-decoration: none;
-            color: #333;
+        .footer-master{
+            margin: 50px 0 30px 0;
         }
-
+        footer a {
+        text-decoration: none; 
+        color: #333; 
+        }
+        
         footer a:hover {
-            color: #007bff;
+        color: #007bff; 
         }
 
         /* botão de logout */
@@ -200,14 +260,10 @@ $conn->close();
     <hr>
     <div class="container mx-auto">
         <h2 class="mt-5">Formulário de atualização</h2>
-        <p>Escolha o respectivo <strong>ID</strong> que deseja alterar. Em seguida, digite os novos dados que deseja
-            atualizar.
-            Ao realizar as alterações necessárias, clique em 'Atualizar', e os dados serão atualizados na tabela acima.
+        <p>Escolha o respectivo <strong>ID</strong> que deseja alterar. Em seguida, digite os novos dados que deseja atualizar.
+            Ao realizar as alterações necessárias, clique em 'Atualizar', e os dados serão atualizados na tabela acima.                        
             <br>
-            <i>As alterações realizadas na tabela serão refletidas após <strong>0,5 segundos.</strong></i>
-            <br>
-            <strong>Todos os campos do formulário de atualização devem ser preenchidos. Caso contrário, o campo não
-                preenchido ficará em branco na tabela.</strong>
+            <strong>Caso queira alterar apenas um campo como, por exemplo, 'nome' digite o respectivo 'ID' é o novo nome, depois clique em atualizar.</strong>
         </p>
         <!-- Formulário para atualizar dados da lista de contatos -->
         <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
@@ -234,34 +290,32 @@ $conn->close();
         <!-- Cadastrar novo contato -->
         <div class="mt-5">
             <h2>Cadastrar novo contato</h2>
-            <p>Caso seja um novo usuário sua lista estará em branco, portanto, é necessário cadastrar novos contatos.
-            </p>
+            <p>Caso seja um novo usuário sua lista estará em branco, portanto, é necessário cadastrar novos contatos.</p>
 
-            <form method="post" action="processar_cadastro.php">
+        <form method="post" action="processar_cadastro.php" onsubmit="return validarFormulario();">
                 <div class="mb-3">
                     <label for="nome_contato" class="form-label">Nome</label>
                     <input type="text" class="form-control" id="nome_contato" name="nome_contato" required>
                 </div>
                 <div class="mb-3">
-                    <label for="sobrenome_contato" class="form-label">Sobrenome</label>
+                    <label for "sobrenome_contato" class="form-label">Sobrenome</label>
                     <input type="text" class="form-control" id="sobrenome_contato" name="sobrenome_contato" required>
                 </div>
                 <div class="mb-3">
                     <label for="email_contato" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="email_contato" name="email_contato" required>
+                    <input type="email" class="form-control" id="email_contato" name="email_contato" required>                    
                 </div>
                 <button type="submit" class="btn btn-primary" style="margin: 5px 0 10px 0;">Cadastrar Contato</button>
-            </form>
-
-        </div>
-        <hr>
+        </form>
+    </div>
+    <hr>
     </div>
 
     <!-- scripts Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
     <script>
-        // Função para ocultar mensagens de sucesso ou erro após 5 segundos
+        // Função para ocultar mensagens de sucesso ou erro após 3 segundos, tanto para o formulário de atualização como cadastro.
         setTimeout(function () {
             var sucessoDiv = document.querySelector('.alert-success');
             var erroDiv = document.querySelector('.alert-danger');
@@ -273,27 +327,21 @@ $conn->close();
             if (erroDiv) {
                 erroDiv.style.display = 'none';
             }
-        }, 2000);
+        }, 3500);
     </script>
 
-    <footer class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-8 text-center">
-                <p class="mb-2">Desenvolvido por:</p>
-                <a href="https://github.com/felipe-ssantos/graduacao-time-line/tree/main/04-semestre-2023/desenvolvimento_web_servidor/pratique-un03"
+    <div class="footer-master">            
+        <footer class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="text-center">
+                    <p class="mb-2">Desenvolvido por:</p>
+                    <a href="https://github.com/felipe-ssantos/graduacao-time-line/tree/main/04-semestre-2023/desenvolvimento_web_servidor/pratique-un03"
                     class="text-decoration-none" target="_blank">
-                    N.F.S.S | 4ª Semestre: Sistemas para Internet | 2023 ©
-                </a>
-                <hr>
-                <p class="row justify-content-center"><br>Este sistema foi desenvolvido exclusivamente para fins
-                    acadêmicos, como parte da disciplina de Desenvolvimento Web Servidor. Todas as informações contidas
-                    nele são fictícias e destinam-se apenas a fins de teste. Por favor, esteja ciente de que, podem
-                    haver a presença de erros no sistema.
-                </p>
+                     N.F.S.S © <?php echo date('Y'); ?>
+                    </a>
+                </div>
             </div>
-        </div>
-    </footer>
-
+        </footer>
+    </div>    
 </body>
-
 </html>
